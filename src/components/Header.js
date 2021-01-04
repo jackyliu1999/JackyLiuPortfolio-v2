3719c53e20871655b7aa8b31bf7026ad7f7274cd
Jacky Liu <nimbuskycloud@gmail.com>
2021-01-03 20:53:37 -0500

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Navbar, Nav, Form, FormControl, Button, Container, Col } from "react-bootstrap";
 
 const Header = () => {
     return (
         <div className="">
-            <header id="header" class="fixed-top">
+            {/* <header id="header" class="fixed-top">
                 <div class="container d-flex align-items-center justify-content-between">
                     <h1 class="logo"><a href="/Home">Jacky<span>.</span></a></h1>
                     <nav class="nav-menu d-none d-lg-block">
@@ -16,7 +17,26 @@ const Header = () => {
                         </ul>
                     </nav>
                 </div>
-            </header>
+            </header> */}
+            <Navbar expand="xl" style={{ background: "black" }} variant="dark" fixed="top" collapseOnSelect>
+                <Container fluid>
+                    <Navbar.Brand href="#home" className="img-container"><div id="header" class="container d-flex align-items-center justify-content-between"><h1 class="logo"><a href="/Home">Jacky<span>.</span></a></h1></div>
+                    </Navbar.Brand>
+                    <Navbar.Toggle className="ml-3 mb-2" aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Col></Col>
+                        <Col md="auto">
+                            <div class="nav-menu d-none d-lg-block">
+                                <Navbar.Text className="m-1"><a href="/Home">Home</a></Navbar.Text>
+                                <Navbar.Text className="m-1"><a href="/Home#projs">Projects</a></Navbar.Text>
+                                <Navbar.Text className="m-1"><a href="Skills">Skills</a></Navbar.Text>
+                                <Navbar.Text className="m-1"><a href="Archives">Archives</a></Navbar.Text>
+                                <Navbar.Text className="m-1"><a href="Resume">Resume</a></Navbar.Text>
+                            </div>
+                        </Col>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     )
 }
